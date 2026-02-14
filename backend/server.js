29f7d2e986f6dbb86921464e0999c84d8aa39c16
Ayush vashisth <ayushvashisth@Blanche.local>
2026-02-14 18:10:53 +0530

    added files

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import errorHandler from "./middleware/error.middleware.js";
+import morgan from "morgan";
+import helmet from "helmet"
 
 const app = express();
 
@@ -14,11 +16,12 @@ app.use(
 );
 
 // app.use(express.json({ limit: "20kb" }));
+app.use(helmet())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "20kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
-
+app.use(morgan('dev'))
 
 
 app.use(errorHandler);
